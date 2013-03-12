ca16d2faaf920dcde6f708dc7c61b3a58e3fa0ed
Vipul Goel <vipulkit@vipulgoel.org>
2013-03-12 11:51:13 +0530

    Added build for css.rs

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-rm -rf *.so *.o testcpp testrs
+rm -rf *.so *.o 
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 gcc -fPIC -shared iconv_wrapper.c -o libiconv_wrapper.so 
 rustc -L . iconv_crate.rc 
 rustc -L . parserutils.rs
 rustc -L . wapcaplet.rs
-
+rustc -L . css.rs
