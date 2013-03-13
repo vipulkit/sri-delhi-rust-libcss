32a9d9ad0836a24e3c9dee3e99855f17ce4ba2a2
Vipul Goel <vipulkit@vipulgoel.org>
2013-03-13 17:14:08 +0530

    Iconv Wrapper optimized and released to version 1.0 as initial stable working release .

--- iconv_wrapper.c
+++ iconv_wrapper.c
@@ -6,7 +6,7 @@
 
 
 
-void showHex(const char * message , const char * s , int len ) 
+void showDex(const char * message , const char * s , int len ) 
 {	int i = 0 ;
 	printf ("\n %s Dec is - ",message);
 	for( i=0 ; i<len ; ++i ) {
@@ -30,8 +30,7 @@ extern void DeallocateBuffer( char * buffer)
 extern uint64_t rust_iconv_open ( const char * to_code, const char * from_code )
 {
 	uint64_t result ;
-	void * handle ;
-	handle = iconv_open("UTF-32BE",from_code);
+	void * handle = iconv_open(to_code,from_code);
 	result = handle ;
 	printf("\n Opened rust iconv =%lld=%lld=%s=%s= \n",result,handle, to_code , from_code );
 	return result ;
@@ -46,25 +45,8 @@ extern size_t rust_iconv(uint64_t handle , char ** inbuf , size_t * insize , cha
 		result = iconv(handle, NULL,0,NULL,0) ;
 	}
 	else {
-		//printf("\n Rust iconv call =%lld=%d=%d= \n",handle,*insize,*outsize );
-		//fflush(stdout);
-		//showHex("Input-",*inbuf, *insize) ;
-		//showHex("Output-",*outbuf,*outsize);
-		//fflush(stdout);
-
 	    void * res = handle ;
-	   
-	    //char * iptr = *inbuf ;
-	    //char * optr = *outbuf ;
-	    //size_t ibytes = *insize ;
-	    //size_t obytes = *outsize ; 
-
 		result = iconv(res,inbuf,insize,outbuf,outsize);
-		//size_t result = iconv(res,&iptr,ibytes,&optr,outsize);
-		//printf("\n After Rust iconv call =%lld=%lld=%lld=%lld= \n",result, handle , *insize, *outsize );
-		//showHex("Output After Processing-",optr,16);
-
-		//fflush(stdout);
 	}
 
 	if (result==(size_t)-1) {
@@ -81,7 +63,6 @@ extern size_t rust_iconv(uint64_t handle , char ** inbuf , size_t * insize , cha
 	else {
 		*error_num = 0 ;
 	}
-	//printf("\n Error code is =%ud= \n",error_num );
 	return result ;
 }
 
