18d0b96802b3b5ee3aef4062fd74608eab67e457
PraveenSethi123 <praveen@13_servo.(none)>
2013-03-26 17:41:12 +0530

    Added Interfaces for Select, Hint and Computed

--- css.rs
+++ css.rs
@@ -247,26 +247,6 @@ pub enum css_stylesheet_params_version {
 	CSS_STYLESHEET_PARAMS_VERSION_1 = 1
 }
 
-
-
-
-
-
-// select.h
-
-pub enum css_pseudo_element {
-	CSS_PSEUDO_ELEMENT_NONE         = 0,
-	CSS_PSEUDO_ELEMENT_FIRST_LINE   = 1,
-	CSS_PSEUDO_ELEMENT_FIRST_LETTER = 2,
-	CSS_PSEUDO_ELEMENT_BEFORE       = 3,
-	CSS_PSEUDO_ELEMENT_AFTER        = 4,
-
-	CSS_PSEUDO_ELEMENT_COUNT	= 5	/* < Number of pseudo elements */
-} 
-// TODO : structs and typedefs
-
-// computed.h
-
 // properties.h
 pub enum css_properties_e {
 	CSS_PROP_AZIMUTH			= 0x000,
@@ -6422,3 +6402,497 @@ impl font_face
 // ===========================================================================================================
 // Font_Face.h and Font_Face.c implementation/data-structs ends here 
 // ===========================================================================================================
+
+
+// ===========================================================================================================
+// CSS-SELECT-HINT implementation/data-structs start here 
+// ===========================================================================================================
+
+/**
+ * Length object for use in presentational hints
+ */
+struct css_hint_length {
+	value:css_fixed,
+	unit:css_unit
+}
+
+/**
+ * Presentational hints
+ */
+struct css_hint {
+	/* Ownership of all data is passed to libcss */
+	data: {
+	 	clip:@css_computed_clip_rect,
+		color:css_color,
+		content:@css_computed_content_item,
+		counter:@css_computed_counter,
+		fixed:css_fixed,
+		integer:i32,
+		length:css_hint_length,
+		position:{
+			h:css_hint_length, 
+			v:css_hint_length
+		} ,
+		string: lwc_string,
+		strings:@lwc_string
+	}, 
+
+	status:u8
+} 
+
+// ===========================================================================================================
+// CSS-SELECT-HINT implementation/data-structs ends here 
+// ===========================================================================================================
+
+// ===========================================================================================================
+// CSS-SELECT-COMPUTED implementation/data-structs start here 
+// ===========================================================================================================
+
+
+struct css_computed_counter {
+	name:@lwc_string,
+	value:css_fixed
+}
+
+
+struct css_computed_clip_rect {
+	top:css_fixed,
+	right:css_fixed,
+	bottom:css_fixed,
+	left:css_fixed,
+
+	tunit:css_unit,
+	runit:css_unit,
+	bunit:css_unit,
+	lunit:css_unit, 
+
+	top_auto:bool,
+	right_auto:bool,
+	bottom_auto:bool,
+	left_auto:bool
+} 
+
+
+struct css_computed_content_item {
+	content_type:u8,
+	date: {
+		string:@lwc_string, 
+		uri:@lwc_string,
+		attr:@lwc_string,
+		counter: {
+			name:@lwc_string,
+			style:u8
+		} ,
+		counters: {
+			name:@lwc_string,
+			sep:@lwc_string,
+			style:u8
+		} 
+	}
+}
+
+
+struct css_computed_uncommon {
+/*
+ * border_spacing		  1 + 2(4)	  2(4)
+ * clip				  2 + 4(4) + 4	  4(4)
+ * letter_spacing		  2 + 4		  4
+ * outline_color		  2		  4
+ * outline_width		  3 + 4		  4
+ * word_spacing			  2 + 4		  4
+ * 				---		---
+ * 				 52 bits	 40 bytes
+ *
+ * Encode counter_increment and _reset as an array of name, value pairs,
+ * terminated with a blank entry.
+ *
+ * counter_increment		  1		  sizeof(ptr)
+ * counter_reset		  1		  sizeof(ptr)
+ * 				---		---
+ * 				  2 bits	  2sizeof(ptr) bytes
+ *
+ * Encode cursor uri(s) as an array of string objects, terminated with a
+ * blank entry.
+ *
+ * cursor			  5		  sizeof(ptr)
+ * 				---		---
+ * 				  5 bits	  sizeof(ptr) bytes
+ *
+ * Encode content as an array of content items, terminated with a blank entry.
+ *
+ * content			  2		  sizeof(ptr)
+ * 				---		---
+ * 				  2 bits	  sizeof(ptr)
+ *
+ * 				___		___
+ * 				 61 bits	 40 + 4sizeof(ptr) bytes
+ *
+ * 				  8 bytes	 40 + 4sizeof(ptr) bytes
+ * 				===================
+ * 				 48 + 4sizeof(ptr) bytes
+ *
+ * Bit allocations:
+ *
+ *    76543210
+ *  1 llllllcc	letter-spacing | outline-color
+ *  2 ooooooob	outline-width  | border-spacing
+ *  3 bbbbbbbb	border-spacing
+ *  4 wwwwwwir	word-spacing   | counter-increment | counter-reset
+ *  5 uuuuu...	cursor         | <unused>
+ *  6 cccccccc	clip
+ *  7 cccccccc	clip
+ *  8 ccccccoo	clip           | content
+ */
+	bits:[u8*8],
+
+	border_spacing:[css_fixed*2],
+
+	clip:[css_fixed*4],
+
+	letter_spacing:css_fixed,
+
+	outline_color:css_color,
+	outline_width:css_fixed,
+
+	word_spacing:css_fixed,
+
+	counter_increment:@css_computed_counter,
+	counter_reset:@css_computed_counter,
+
+	cursor:@lwc_string,
+
+	content:@css_computed_content_item
+} 
+
+
+ struct css_computed_page {
+/*
+ * Bit allocations:
+ *
+ *    76543210
+ *  1 aaabbbii	page_break_after | page_break_before | page_break_inside
+ *  2 ......wo	widows  | orphans
+ */
+	bits:[u8*2],
+	
+	widows:css_fixed,
+	orphans:css_fixed
+}
+
+
+
+struct css_computed_style {
+/*
+ * background_attachment	  2
+ * background_repeat		  3
+ * border_collapse		  2
+ * border_top_style		  4
+ * border_right_style		  4
+ * border_bottom_style		  4
+ * border_left_style		  4
+ * caption_side			  2
+ * clear			  3
+ * direction			  2
+ * display			  5
+ * empty_cells			  2
+ * float			  2
+ * font_style			  2
+ * font_variant			  2
+ * font_weight			  4
+ * list_style_position		  2
+ * list_style_type		  4
+ * overflow			  3
+ * outline_style		  4
+ * position			  3
+ * table_layout			  2
+ * text_align			  4
+ * text_decoration		  5
+ * text_transform		  3
+ * unicode_bidi			  2
+ * visibility			  2
+ * white_space			  3
+ *				---
+ *				 84 bits
+ *
+ * Colours are 32bits of AARRGGBB
+ * Dimensions are encoded as a fixed point value + 4 bits of unit data
+ *
+ * background_color		  2		  4
+ * background_image		  1		  sizeof(ptr)
+ * background_position		  1 + 2(4)	  2(4)
+ * border_top_color		  2		  4
+ * border_right_color		  2		  4
+ * border_bottom_color		  2		  4
+ * border_left_color		  2		  4
+ * border_top_width		  3 + 4		  4
+ * border_right_width		  3 + 4		  4
+ * border_bottom_width		  3 + 4		  4
+ * border_left_width		  3 + 4		  4
+ * top				  2 + 4		  4
+ * right			  2 + 4		  4
+ * bottom			  2 + 4		  4
+ * left				  2 + 4		  4
+ * color			  1		  4
+ * font_size			  4 + 4		  4
+ * height			  2 + 4		  4
+ * line_height			  2 + 4		  4
+ * list_style_image		  1		  sizeof(ptr)
+ * margin_top			  2 + 4		  4
+ * margin_right			  2 + 4		  4
+ * margin_bottom		  2 + 4		  4
+ * margin_left			  2 + 4		  4
+ * max_height			  2 + 4		  4
+ * max_width			  2 + 4		  4
+ * min_height			  1 + 4		  4
+ * min_width			  1 + 4		  4
+ * padding_top			  1 + 4		  4
+ * padding_right		  1 + 4		  4
+ * padding_bottom		  1 + 4		  4
+ * padding_left			  1 + 4		  4
+ * text_indent			  1 + 4		  4
+ * vertical_align		  4 + 4		  4
+ * width			  2 + 4		  4
+ * z_index			  2		  4
+ * 				---		---
+ *				181 bits	140 + 2sizeof(ptr) bytes
+ *
+ * Encode font family as an array of string objects, terminated with a 
+ * blank entry.
+ *
+ * font_family			  3		  sizeof(ptr)
+ * 				---		---
+ * 				  3 bits	  sizeof(ptr)
+ *
+ * Encode quotes as an array of string objects, terminated with a blank entry.
+ *
+ * quotes			  1		  sizeof(ptr)
+ * 				---		---
+ * 				  1 bit		  sizeof(ptr) bytes
+ *
+ * 				___		___
+ *				269 bits	140 + 4sizeof(ptr) bytes
+ *
+ *				 34 bytes	140 + 4sizeof(ptr) bytes
+ *				===================
+ *				174 + 4sizeof(ptr) bytes
+ *
+ * Bit allocations:
+ *
+ *    76543210
+ *  1 vvvvvvvv	vertical-align
+ *  2 ffffffff	font-size
+ *  3 ttttttti	border-top-width    | background-image
+ *  4 rrrrrrrc	border-right-width  | color
+ *  5 bbbbbbbl	border-bottom-width | list-style-image
+ *  6 lllllllq	border-left-width   | quotes
+ *  7 ttttttcc	top                 | border-top-color
+ *  8 rrrrrrcc	right               | border-right-color
+ *  9 bbbbbbcc	bottom              | border-bottom-color
+ * 10 llllllcc	left                | border-left-color
+ * 11 hhhhhhbb	height              | background-color
+ * 12 llllllzz	line-height         | z-index
+ * 13 ttttttbb	margin-top          | background-attachment
+ * 14 rrrrrrbb	margin-right        | border-collapse
+ * 15 bbbbbbcc	margin-bottom       | caption-side
+ * 16 lllllldd	margin-left         | direction
+ * 17 mmmmmmee	max-height          | empty-cells
+ * 18 mmmmmmff	max-width           | float
+ * 19 wwwwwwff	width               | font-style
+ * 20 mmmmmbbb	min-height          | background-repeat
+ * 21 mmmmmccc	min-width           | clear
+ * 22 tttttooo	padding-top         | overflow
+ * 23 rrrrrppp	padding-right       | position
+ * 24 bbbbbo..	padding-bottom      | opacity               | <unused>
+ * 25 lllllttt	padding-left        | text-transform
+ * 26 tttttwww	text-indent         | white-space
+ * 27 bbbbbbbb	background-position
+ * 28 bdddddff	background-position | display               | font-variant
+ * 29 tttttfff	text-decoration     | font-family
+ * 30 ttttrrrr	border-top-style    | border-right-style
+ * 31 bbbbllll	border-bottom-style | border-left-style
+ * 32 ffffllll	font-weight         | list-style-type
+ * 33 oooottuu	outline-style       | table-layout          | unicode-bidi
+ * 34 vvlltttt	visibility          | list-style-position   | text-align
+ */
+	bits:[u8*34],
+
+	unused:[u8*2],
+
+	background_color:css_color,
+	background_image:@lwc_string,
+	background_position:[css_fixed*2],
+
+	border_color:[css_color*4],
+	border_width:[css_fixed*4],
+
+	top:css_fixed,
+	right:css_fixed,
+	bottom:css_fixed,
+	left:css_fixed,
+
+	color:css_color,
+
+	font_size:css_fixed,
+
+	height:css_fixed,
+
+	line_height:css_fixed,
+
+	list_style_image:@lwc_string,
+
+	margin:[css_fixed*4],
+
+	max_height:css_fixed,
+	max_width:css_fixed,
+
+	min_height:css_fixed, 
+	min_width:css_fixed,
+
+	opacity:css_fixed,
+
+	padding:[css_fixed*4],
+
+	text_indent:css_fixed,
+
+	vertical_align:css_fixed,
+
+	width:css_fixed,
+
+	z_index:i32, 
+
+	font_family:@lwc_string,
+
+	quotes:@lwc_string, 
+
+	uncommon:@css_computed_uncommon,/**< Uncommon properties */
+	aural:~[u8],			/**< Aural properties */
+	page:@css_computed_page	//< Page properties */
+
+	
+}
+
+// ===========================================================================================================
+// CSS-SELECT-COMPUTED implementation/data-structs ends here 
+// ===========================================================================================================
+
+// ===========================================================================================================
+// CSS-SELECT implementation/data-structs start here 
+// ===========================================================================================================
+
+
+enum css_pseudo_element {
+	CSS_PSEUDO_ELEMENT_NONE         = 0,
+	CSS_PSEUDO_ELEMENT_FIRST_LINE   = 1,
+	CSS_PSEUDO_ELEMENT_FIRST_LETTER = 2,
+	CSS_PSEUDO_ELEMENT_BEFORE       = 3,
+	CSS_PSEUDO_ELEMENT_AFTER        = 4,
+
+	CSS_PSEUDO_ELEMENT_COUNT	= 5	//< Number of pseudo elements 
+}
+struct css_select_results {
+	/**
+	 * Array of pointers to computed styles, 
+	 * indexed by css_pseudo_element. If there
+	 * was no styling for a given pseudo element, 
+	 * then no computed style will be created and
+	 * the corresponding pointer will be set to NULL
+	 */
+	styles:@[@css_computed_style] //[CSS_PSEUDO_ELEMENT_COUNT];
+}
+
+enum css_select_handler_version {
+	CSS_SELECT_HANDLER_VERSION_1 = 1
+}
+
+struct css_select_handler {
+	/** ABI version of this structure */
+	handler_version: u32,
+
+	node_name:@fn (pw:~[u8], node:~[u8],qname:~css_qname)->css_result,
+
+	node_classes: @fn(pw:~[u8], node:~[u8],	classes:~[@lwc_string],n_classes:@u32)->css_result,
+
+	node_id: @fn (pw:~[u8], node:~[u8],id:@lwc_string ) -> css_result,
+
+	named_ancestor_node:@fn (pw:~[u8], node:~[u8], qname:@css_qname, ancestor:~[u8]) -> css_result,
+
+	named_parent_node:@fn (pw:~[u8], node:~[u8],qname:@css_qname, parent:~[u8]) -> css_result,
+
+	named_sibling_node:@fn (pw:~[u8], node:~[u8], qname:@css_qname, sibling:~[u8]) -> css_result,
+
+	named_generic_sibling_node:@fn (pw:~[u8], node:~[u8], qname:@css_qname, sibling:~[u8]) -> css_result,
+
+	parent_node:@fn (pw:~[u8], node:~[u8], parent:~[u8])->css_result,
+
+	sibling_node:@fn(pw:~[u8], node:~[u8], sibling:~[u8]) -> css_result,
+
+	node_has_name:@fn (pw:~[u8], node:~[u8],qname:@css_qname, matched:bool) -> css_result,
+
+	node_has_class:@fn (pw:~[u8], node:~[u8],name:@lwc_string , matched:bool) -> css_result,
+
+	node_has_id:@fn (pw:~[u8], node:~[u8],name:@lwc_string , matched:bool)-> css_result,
+
+	node_has_attribute:@fn(pw:~[u8], node:~[u8], qname:@css_qname, matched:bool) -> css_result,
+
+	node_has_attribute_equal:@fn(pw:~[u8], node:~[u8], qname:@css_qname, value:@lwc_string, matched:bool) -> css_result,
+
+	node_has_attribute_dashmatch: @fn (pw:~[u8], node:~[u8], qname:@css_qname, value:@lwc_string, matched:bool) -> css_result,
+
+	node_has_attribute_includes:@fn (pw:~[u8], node:~[u8],	qname:@css_qname, value:@lwc_string, matched:bool) -> css_result,
+
+	node_has_attribute_prefix: @fn(pw:~[u8], node:~[u8],qname:@css_qname, value:@lwc_string, matched:bool) -> css_result,
+
+	node_has_attribute_suffix: @fn (pw:~[u8], node:~[u8], qname:@css_qname, value:@lwc_string, matched:bool) -> css_result,
+
+	node_has_attribute_substring: @fn (pw:~[u8], node:~[u8], qname:@css_qname, value:@lwc_string, matched:bool) -> css_result,
+
+	node_is_root:@fn (pw:~[u8], node:~[u8], matched:bool) -> css_result,
+
+	node_count_siblings:@fn (pw:~[u8], node:~[u8], same_name:bool, after:bool, count:@i32) -> css_result,
+
+	node_is_empty:@fn (pw:~[u8], node:~[u8], matched:bool)-> css_result,
+
+	node_is_link:@fn (pw:~[u8], node:~[u8], matched:bool)-> css_result,
+
+	node_is_visited:@fn (pw:~[u8], node:~[u8], matched:bool)-> css_result,
+
+	node_is_hover:@fn (pw:~[u8], node:~[u8], matched:bool)-> css_result,
+
+	node_is_active:@fn (pw:~[u8], node:~[u8], matched:bool)-> css_result,
+
+	node_is_focus:@fn (pw:~[u8], node:~[u8], matched:bool)-> css_result,
+
+	node_is_enabled:@fn (pw:~[u8], node:~[u8], matched:bool)-> css_result,
+
+	node_is_disabled:@fn (pw:~[u8], node:~[u8], matched:bool)-> css_result,
+
+	node_is_checked:@fn (pw:~[u8], node:~[u8], matched:bool)-> css_result,
+
+	node_is_target:@fn (pw:~[u8], node:~[u8], matched:bool)-> css_result,
+
+	node_is_lang:@fn (pw:~[u8], node:~[u8],value:@lwc_string, matched:bool)-> css_result,
+
+	node_presentational_hint:@fn (pw:~[u8], node:~[u8], property:u32, hint:@css_hint)-> css_result,
+
+	ua_default_for_property:@fn (pw:~[u8], property:u32, hint:@css_hint)-> css_result,
+
+	compute_font_size:@fn (pw:~[u8], parent:@css_hint, size:@css_hint)-> css_result
+
+}
+/*
+pub struct css_select {
+	
+}
+
+pub fn lcss_select(sheet:@css_stylesheet)->@css_select {
+	
+				let	css_select_instance = @css_select ;
+
+	return css_select_instance;
+}
+
+impl css_select
+{
+
+}
+*/
