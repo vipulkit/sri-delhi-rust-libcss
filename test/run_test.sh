86f59a912e2c1303c32c72227cf9030feb1a6ccf
sushanta.k <sushanta@13_servo.(none)>
2013-04-17 16:17:56 +0530

    - New files added for better test experience

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,7 +1,17 @@
+
 # run test programs 
-./test_aliases 
-./test_wapcaplet
-./test_riconv
-./test_parserutils 
-./test_parserutils_filter 
-./test_parserutils_inputstream ./utf16.txt 
+
+#./test_aliases 
+#./test_wapcaplet
+#./test_riconv
+#./test_parserutils 
+#./test_parserutils_filter 
+#./test_parserutils_inputstream ./utf16.txt 
+
+./test_csdtect   utf32.txt
+./test_csdtect   utf16.txt
+./test_csdtect   utf8.txt
+./test_csdtect   eucjp.txt 
+./test_csdtect   chinese.txt 
+
+chmod 777 *.csv
