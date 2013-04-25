d98581510bef7066267509725893663957e56ae0
abhijeetgaiha <abhijeet.gaiha@gmail.com>
2013-04-25 17:04:33 +0530

    Added parallel string internment API in libwapcaplet

--- make.sh
+++ make.sh
@@ -8,6 +8,7 @@ rustc -L . csdetect.rs
 rustc -L . parserutils_filter.rs
 rustc -L . parserutils_inputstream.rs
 rustc -L . wapcaplet.rs
+rustc -L . css_propstrings.rs
 rustc -L . css_enum.rs
 #rustc -L . css_ds.rs
 rustc -L . css_lexer.rs
