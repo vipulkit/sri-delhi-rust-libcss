c68cb81f322131cb920b6fd287c64ceeca9c0555
Vipul Goel <vipulkit@vipulgoel.org>
2013-04-30 17:37:20 +0530

    Initial commit for selector module of libcss

---
+++ css_select_const.rs
@@ -0,0 +1,6 @@
+#[link(name = "css_select_const", vers = "0.1")];
+#[crate_type = "lib"];
+
+static LETTER_SPACING_INDEX : int = 0;
+static LETTER_SPACING_SHIFT : int = 2;
+static LETTER_SPACING_MASK  : int = 0xfc;
