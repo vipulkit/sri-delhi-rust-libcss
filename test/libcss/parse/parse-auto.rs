dc4ad60d954f994d7c433766796121b2aafd4f6f
iampiyush <pshagl007@gmail.com>
2013-05-18 11:33:37 +0530

    minor changes

--- test/libcss/parse/parse-auto.rs
+++ test/libcss/parse/parse-auto.rs
@@ -1,24 +1,20 @@
 extern mod std;
 extern mod css;
 extern mod wapcaplet;
-// extern mod test;
 
-// use test::*;
 use core::io::*;
 use std::arc;
 use css::css::*;
 use css::css::css::*;
 use css::stylesheet::*;
 use css::utils::errors::*;
-// use css::include::types::*;
 use wapcaplet::*;
-
 
 pub fn resolve_url(_:~str, rel:arc::RWARC<~lwc_string>) -> (css_result,Option<arc::RWARC<~lwc_string>>) {
 	return (CSS_OK,Some(rel.clone()));
 }
 
-pub fn fill_params() -> css_params {
+fn fill_params() -> css_params {
 	let css_param = css_params {
 		params_version : 1,
 		level: CSS_LEVEL_21,
@@ -35,56 +31,97 @@ pub fn fill_params() -> css_params {
 	return css_param;
 }
 
-fn main() {
-    io::println("parse");
-}
-
 fn css_create_fn() -> ~css{
 	let css = css_create(fill_params());
 	css
 }
 
-fn parse(file_name: ~str) {
-
-	let mut css = css_create_fn();
-	let CHUNK_SIZE = 10;
-	let mut buf: ~[u8];
-	let r:@Reader = io::file_reader(&Path(file_name)).get(); 
-	r.seek(0 , SeekEnd);
-	let mut len = r.tell();
-	r.seek(0 , SeekSet);
-
-	while len>0 {
-		buf = r.read_bytes(CHUNK_SIZE as uint);
-		len -= buf.len();
-		let error = css.css_stylesheet_append_data(buf);
-		match error {
-			CSS_OK => {},
-			CSS_NEEDDATA => {},
-			_ => {assert!(false);}
-		}
-	}
-	buf = r.read_bytes(len as uint);
-	let error = css.css_stylesheet_append_data(buf);
-	match error {
-		CSS_OK => {},
-		CSS_NEEDDATA => {},
-		_ => {assert!(false);}
-	}
-	let css_stylesheet = css.css_stylesheet_data_done();
-
-	match css_stylesheet.url {
-		~"foo" => {},
-		_ => {assert!(false);}
-	}
-	match css_stylesheet.title {
-		~"" => {},
-		_ => {assert!(false);}
-	}
+fn main() {
+    io::println("parse");
+    parse(~"../data/parse/atrules.dat");
 }
 
-//  validate rules here
+fn parse(file_name: ~str) {
+	let mut css = css_create_fn();
+	let r:@Reader = io::file_reader(&Path(file_name)).get();
+	let mut dataFlag = false;
+	let mut expectedFlag = false;
+	let mut final_buf: ~[u8];
+	let mut expected_buf: ~[u8];
 
+	while !r.eof() {
+		let buf = r.read_line();
+		if buf == ~"#data" {
+			dataFlag = true;
+			expectedFlag = false; 
+		}
+		else if buf == ~"#errors" {
+			dataFlag = false;
+			expectedFlag = false;
+		}
+		else if buf == ~"#expected" {
+			expectedFlag = true;
+			dataFlag = false;
+
+		}
+		else if buf == ~"#reset" {
+			dataFlag = false;
+			expectedFlag = false;
+			expected_buf = ~[];
+		}
+		else if dataFlag {
+			final_buf = ~[];
+			for str::each_char(buf) |i| {
+				final_buf.push(i as u8);
+			}
+			vec::reverse(final_buf);
+		}
+
+		if expectedFlag && !expected_buf.is_empty() {
+			for str::each_char(buf) |i| {
+				expected_buf.push(i as u8);
+			}
+			vec::reverse(expected_buf);
+		}
+
+		if dataFlag && !final_buf.is_empty() {
+			let error = css.css_stylesheet_append_data(final_buf);
+			match error {
+				CSS_OK => {},
+				CSS_NEEDDATA => {},
+				_ => {assert!(false);}
+			}
+			let (error , css_stylesheet) = css.css_stylesheet_data_done();
+
+			match error {
+				CSS_OK => {},
+				_ => {assert!(false);}
+			}
+
+			let rule =  css_stylesheet.unwrap().rule_list;
+			loop {
+				match rule {
+					None => break,
+					Some(x) => {
+						match x {
+							RULE_SELECTOR(x) => {
+								// x.selectors[]
+								// to be done after select module
+							},
+							RULE_CHARSET(_) => {
+								//no fail condition
+							},
+							RULE_IMPORT(_) => {
+								//no fail condition
+							}
+							_ => {}
+						}
+					}
+				}
+			}
+		}
+	}
+}
 
 
 #[test]
