765a7325896bf90999456970c183d3a84802573f
Vipul Goel <vipulkit@vipulgoel.org>
2013-05-20 12:31:20 +0530

    added elevation empty_cells in select/prop

--- libcss/select/properties/properties.rs
+++ libcss/select/properties/properties.rs
@@ -2534,4 +2534,119 @@ pub fn css__compose_display(parent:@mut css_computed_style,
 
 ///////////////////////////////////////////////////////////////////
 
+// elevation
+///////////////////////////////////////////////////////////////////
+pub fn css__cascade_elevation(opv:u32, style:@mut css_style, 
+									state:@mut css_select_state) -> css_result {
 
+	let mut val :i32  = 0;
+	let mut unit : u32 = UNIT_DEG ;
+
+	if (isInherit(opv) == false) {
+		match (getValue(opv)) {
+			ELEVATION_ANGLE =>{
+				val = peek_bytecode(style) as i32 ;
+				advance_bytecode(style);
+
+				unit = peek_bytecode(style);
+				advance_bytecode(style);
+			},
+			ELEVATION_BELOW |
+			ELEVATION_LEVEL |
+			ELEVATION_ABOVE |
+			ELEVATION_HIGHER |
+			ELEVATION_LOWER => {},
+				/* \todo convert to public values */
+			_=>{}
+		}
+	}
+
+	//unit = css__to_css_unit(unit);
+
+	if (css__outranks_existing(getOpcode(opv) as u16, isImportant(opv), state,
+			isInherit(opv))) {
+		
+	}
+	CSS_OK
+}
+
+pub fn css__set_elevation_from_hint(_:@mut  css_hint, 
+										_:@mut css_computed_style
+										) -> css_result {
+
+	CSS_OK
+}
+
+pub fn css__initial_elevation(_:@mut css_select_state) -> css_result {
+
+	CSS_OK
+}
+
+pub fn css__compose_elevation(_:@mut css_computed_style,
+									_:@mut css_computed_style,
+									_:@mut css_computed_style
+									) -> css_result {
+
+	CSS_OK
+}
+
+///////////////////////////////////////////////////////////////////
+
+// empty_cells
+///////////////////////////////////////////////////////////////////
+pub fn css__cascade_empty_cells(opv:u32, _:@mut css_style, 
+									state:@mut css_select_state) -> css_result {
+
+	let mut value = CSS_EMPTY_CELLS_INHERIT as u16;
+
+	if (isInherit(opv) == false) {
+		match (getValue(opv)) {
+			EMPTY_CELLS_SHOW => {
+				value = CSS_EMPTY_CELLS_SHOW as u16;
+			},
+			EMPTY_CELLS_HIDE => {
+				value = CSS_EMPTY_CELLS_HIDE as u16;
+			},
+			_=>{}
+		}
+	}
+
+	if (css__outranks_existing(getOpcode(opv) as u16, isImportant(opv), state,
+			isInherit(opv))) {
+		set_empty_cells(state.computed, value as u8);
+	}
+
+	CSS_OK
+}
+
+pub fn css__set_empty_cells_from_hint(hint:@mut  css_hint, 
+										style:@mut css_computed_style
+										) -> css_result {
+
+	set_empty_cells(style, hint.status);
+	CSS_OK
+}
+
+pub fn css__initial_empty_cells(state:@mut css_select_state) -> css_result {
+
+
+	set_empty_cells(state.computed, (CSS_EMPTY_CELLS_SHOW as u8) );
+	CSS_OK
+}
+
+pub fn css__compose_empty_cells(parent:@mut css_computed_style,
+									child:@mut css_computed_style,
+									result:@mut css_computed_style
+									) -> css_result {
+
+	let mut ftype = css_computed_empty_cells(child);
+
+	if (ftype == (CSS_EMPTY_CELLS_INHERIT as u8) ) {
+		ftype = css_computed_empty_cells(parent);
+	}
+
+	set_empty_cells(result, ftype);
+	CSS_OK
+}
+
+///////////////////////////////////////////////////////////////////
