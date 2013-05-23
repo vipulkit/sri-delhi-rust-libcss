d3e572dcec4ea11cd1a4966a3b2d8aa9c36ce3a9
Ryan Choi <ryan.choi@samsung.com>
2013-05-23 14:28:59 +0530

    Updated: testcases

--- test/testcases-ryanc/libwapcaplet/basictests.rs
+++ test/testcases-ryanc/libwapcaplet/basictests.rs
@@ -116,8 +116,13 @@ fn ops_with_a_filled_context() {
 
         // lwc_intern_substring
         let new_hre = l.lwc_intern_string(~"hre");
-        let sub_hre = l.lwc_intern_substring(new_hre.clone(), 1, 3);
-        assert!(l.lwc_string_isequal(new_hre.clone(), sub_hre.clone()));
+        let sub_hre_option = l.lwc_intern_substring(new_hre.clone(), 1, 3);
+        match sub_hre_option {
+            Some(sub_hre) => {
+                assert!(l.lwc_string_isequal(new_hre.clone(), sub_hre.clone()));
+            }
+            None => {}
+        } 
 
         // lwc_string_ref_ok
         // FIXME: needed but not implemented!!!
