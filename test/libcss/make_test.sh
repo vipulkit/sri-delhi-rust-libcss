399be499232c9b98a443d3ad9bd61cb2a7375869
iampiyush <pshagl007@gmail.com>
2013-05-23 20:07:07 +0530

    make_test updated for test/libcss

--- test/libcss/make_test.sh
+++ test/libcss/make_test.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
+
 # do clean
-rm *css21 *csdetect *parse *parse-auto *parse2-auto *lex *number *lex-auto 
-rustc --test -L ../../libparserutils -L ../../libcss -L ../../libwapcaplet css21.rs -L .
-rustc --test -L ../../libparserutils -L ../../libcss -L ../../libwapcaplet parse.rs -L .
-rustc --test -L ../../libparserutils -L ../../libcss -L ../../libwapcaplet parse-auto.rs -L .
-rustc --test -L ../../libparserutils -L ../../libcss -L ../../libwapcaplet parse2-auto.rs -L .
-rustc --test -L ../../libparserutils -L ../../libcss -L ../../libwapcaplet lex.rs -L .
-rustc --test -L ../../libparserutils -L ../../libcss -L ../../libwapcaplet lex-auto.rs -L .
-rustc --test -L ../../libparserutils/ csdetect.rs -L .
-rustc --test -L ../../libparserutils -L ../../libwapcaplet/ -L ../../libcss number.rs -L .
+
+rm *css21 *csdetect *parse *parse-auto *parse2-auto *lex *number *lex-auto
+rustc -L ../../libparserutils -L ../../libcss -L ../../libwapcaplet testutils.rs
+rustc --test -L ../../libparserutils -L ../../libcss -L ../../libwapcaplet css21.rs
+rustc --test -L ../../libparserutils -L ../../libcss -L ../../libwapcaplet parse.rs
+rustc --test -L ../../libparserutils -L ../../libcss -L ../../libwapcaplet parse-auto.rs
+rustc --test -L ../../libparserutils -L ../../libcss -L ../../libwapcaplet parse2-auto.rs
+rustc --test -L ../../libparserutils -L ../../libcss -L ../../libwapcaplet lex.rs
+rustc --test -L ../../libparserutils -L ../../libcss -L ../../libwapcaplet -L . lex-auto.rs
+rustc --test -L ../../libparserutils/  -L ../../libcss -L ../../libwapcaplet -L . csdetect.rs
+rustc --test -L ../../libparserutils -L ../../libwapcaplet/ -L ../../libcss number.rs
