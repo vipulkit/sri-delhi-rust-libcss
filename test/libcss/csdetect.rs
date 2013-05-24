2d43c31f5c5f54d89eb8e46c5d15bb522bd7546f
abhijeetgaiha <abhijeet.gaiha@gmail.com>
2013-05-24 10:14:22 +0530

    Fixed bug in csdetect (bom-charset.dat test case passes)

--- test/libcss/csdetect.rs
+++ test/libcss/csdetect.rs
@@ -3,21 +3,13 @@ extern mod std;
 extern mod testutils;
 extern mod parserutils ; 
 use parserutils::charset::aliases::*;
-use parserutils::input::parserutils_filter::* ;
 use parserutils::charset::csdetect::*;
 
 use testutils::*;
 use core::str::*;
 use std::arc;
-use core::io::*;
-
-fn main() {
-    io::println("csdetect");
-}
-
 
 fn testMain(fileName: ~str) {
-	// io::println(~"testMain : "+ fileName);
 	let len = css__parse_filesize(copy fileName);
 	if len ==0 {
 		return;
@@ -33,16 +25,14 @@ fn testMain(fileName: ~str) {
 			inenc:false
 		};
 	ctx.buf.push(0);//why?
-	//ctx.enc += ~"\0";
-	//ctx.enc.push_char(0);
-	assert!(css__parse_testfile(copy fileName, ~handle_line, CSDETECT(ctx)) == true);
-	if (ctx.bufused > 0 && ctx.buf[ctx.bufused - 1] == '\n' as u8)
-		{ctx.bufused -= 1;}
 
-	run_test(copy ctx.buf, ctx.bufused, copy ctx.enc);
+	assert!(css__parse_testfile(copy fileName, ~handle_line, CSDETECT(ctx)));
+
 }
 
 pub fn handle_line(data:~str, pw:LINE_CTX_DATA_TYPE)-> bool {
+	let mut result = true;
+
 	let ctx :@mut line_ctx_csdetect;
 
 	match pw { CSDETECT(x) => ctx = x, LEX(_) => fail!(~"In File csdetect.rs, Function handle_line, argument LINE_CTX_DATA_TYPE contains incorrect struct line_ctx_lex") };
@@ -56,9 +46,9 @@ pub fn handle_line(data:~str, pw:LINE_CTX_DATA_TYPE)-> bool {
 			if (ctx.buf[ctx.bufused - 1] == '\n' as u8) {
 				ctx.bufused -= 1;	
 			}
-			run_test(copy ctx.buf, ctx.bufused,copy  ctx.enc);	
+			result = run_test(copy ctx.buf, ctx.bufused,copy  ctx.enc);	
             ctx.buf[0]=0;
-            ctx.enc=~"";//[0]=0;
+            ctx.enc=~"";
             ctx.bufused =0;
 		}
 		ctx.indata = str::eq(&data.slice(1,data.len()).to_owned().to_lower(),&~"data");
@@ -81,42 +71,38 @@ pub fn handle_line(data:~str, pw:LINE_CTX_DATA_TYPE)-> bool {
 		}
 	}
 
-    return true;
+    return result;
 }
 
-
-pub fn run_test(data:~[u8],  _:uint, expectedEncoding:~str) {
-	// io::println("inside csdetect run_test");
-    // io::println(~"data = "+ from_bytes(*data));
-    // io::println(~"expectedEncoding = "+expectedEncoding);
+pub fn run_test(data:~[u8],  _:uint, expected_encoding:~str) -> bool {
+    let alias_instance = alias();
+    
+    let expected_mibenum = 
+    	arc::get(&alias_instance).parserutils_charset_mibenum_from_name(copy expected_encoding);
     
     let mut mibenum:u16 = 0;
-    let mut failCount:uint = 0;
+    let alias_instance = alias();
 
-    match parserutils_filter(alias() ,copy expectedEncoding) {
-        (x,PARSERUTILS_OK) =>{
-            let mut filter_instance = x.unwrap();
-            let (charsetOption,srcOption,error)= css__charset_extract(&data, mibenum, CSS_CHARSET_DEFAULT, filter_instance.instance.clone());
-            assert!(match error {
-                PARSERUTILS_OK=>true,
-                _=>false
-            }==true);
-            mibenum = charsetOption.unwrap();
-            //io::println(arc::get(&filter_instance.instance).parserutils_charset_mibenum_to_name(mibenum).unwrap());
-            assert!(mibenum != 0);
-            if !(mibenum == arc::get(&filter_instance.instance).parserutils_charset_mibenum_from_name(to_upper(copy expectedEncoding))) {
-                io::print("fail::");
-                failCount += 1;
-            }   
-            io::println(fmt!(" Detected mibenum %?   Expected %? ",mibenum,arc::get(&filter_instance.instance).parserutils_charset_mibenum_from_name(to_upper(copy expectedEncoding))));
+    let (charsetOption,srcOption,error)= css__charset_extract(&data, mibenum, CSS_CHARSET_DEFAULT, alias_instance.clone());
+    assert!(match error {
+        PARSERUTILS_OK=>true,
+        _=>false
+    }==true);
+    mibenum = charsetOption.unwrap();
 
-            io::println(fmt!(" Detected charset=( %?) mibenum=(%?) Source %? Expected charset=(%?) mibenum=(%?)",arc::get(&filter_instance.instance).parserutils_charset_mibenum_to_name(mibenum).unwrap(),mibenum,srcOption.unwrap(),expectedEncoding,arc::get(&filter_instance.instance).parserutils_charset_mibenum_from_name(to_upper(copy expectedEncoding))));
-           
-        },
-        
-        (_ , _) => fail!() 
+    assert!(mibenum != 0);
+
+    let detected_charset = 
+    	arc::get(&alias_instance).parserutils_charset_mibenum_to_name(mibenum).unwrap();
+
+    io::println(fmt!(" Detected mibenum == %?, Expected mibenum == %? ", mibenum, expected_mibenum));
+    io::println(fmt!(" Detected charset == %?, Source == %? Expected charset ==%?",detected_charset,srcOption.unwrap(), expected_encoding));    
+
+    if !(mibenum == expected_mibenum) {
+        return false;
     }
-    assert!(failCount == 0);
+
+    true
 }
 
 /*#[test]
