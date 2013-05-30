3f1c1c8cf646fc314e1b7896e0b8c88a5e5d2d1b
Ryan Choi <ryan.choi@samsung.com>
2013-05-30 11:21:40 +0900

    Testcases added

--- test/testcases-ryanc/libcss/csdetect.rs
+++ test/testcases-ryanc/libcss/csdetect.rs
@@ -17,22 +17,108 @@ FIXME
 
 */
 
-
-
 extern mod std;
 extern mod css;
 extern mod parserutils;
+extern mod testutils;
 
-
-//use std::arc;
-//use css::parse::*;
-//use parserutils::charset::csdetect::*;
+use parserutils::charset::aliases::*;
+use parserutils::utils::errors::*;
+use css::charset::csdetect::*;
+use testutils::*;
+use std::arc;
 
 fn main() {
     io::println("csdetect");
 }
 
 #[test]
-fn csdetect() {
-    //css__parse_filesize();
+fn bom_charset() {
+    csdetect(~"data/csdetect/bom-charset.dat");
+}
+
+// #[test]
+// fn bom() {
+    // csdetect(~"data/csdetect/bom.dat");
+// }
+
+fn csdetect(filename: ~str) {
+    let len = css__parse_filesize(filename);
+    if len == 0 {
+        return;
+    }
+
+    let ctx = @mut line_ctx_csdetect {
+        buflen: len,
+        bufused: 0,
+        buf: ~[],
+        enc: ~"",
+        indata: false,
+        inenc: false
+    };
+
+    assert!(css__parse_testfile(filename, ~handle_line, Csdetect(ctx)));
+
+    /* and run final test */
+    // ryanc: the last testcase
+    run_test(copy ctx.buf, copy ctx.enc);
+}
+
+fn handle_line(data: ~str, pw: line_ctx) -> bool {
+    let ctx: @mut line_ctx_csdetect;
+    match pw {
+        Csdetect(x) => {ctx = x},
+        _ => {fail!(~"Type mismatch")}
+    };
+
+    if data.char_at(0) == '#' {
+        if ctx.inenc {
+            run_test(copy ctx.buf, copy ctx.enc);
+            ctx.buf = ~[];
+            ctx.enc = ~"";
+            ctx.bufused=0;
+        }
+
+        let line = data.slice(1,data.len()).to_owned().to_lower();
+        ctx.indata = str::eq(&line, &~"data");
+        ctx.inenc = str::eq(&line, &~"encoding");
+    }
+    else {
+        if ctx.indata {
+            ctx.buf += data.to_bytes();
+            ctx.bufused+=data.len();
+        }
+        if ctx.inenc {
+            ctx.enc = data;
+        }
+    }
+
+    return true;
+}
+
+fn run_test(data: ~[u8], expected: ~str) {
+    let mut mibenum: u16 = 0;
+    let source = CSS_CHARSET_DEFAULT;
+
+    let alias = alias();
+    let (charsetOption, srcOption, status) =
+        css__charset_extract(&data, mibenum, source as int, alias.clone());
+    match status {
+        PARSERUTILS_OK => {},
+        _ => {assert!(false);}
+    }
+    mibenum = charsetOption.unwrap();
+
+    assert!(mibenum != 0);
+
+    let detected_charset =
+        arc::get(&alias).parserutils_charset_mibenum_to_name(mibenum).unwrap();
+    let expected_mibenum =
+        arc::get(&alias).parserutils_charset_mibenum_from_name(copy expected);
+
+    io::println(fmt!("Detected charset %s (%?) Source %d Expected %s (%?)",
+                     detected_charset, mibenum,
+                     srcOption.unwrap(), expected, expected_mibenum));
+
+    assert!(mibenum == expected_mibenum);
 }
---
+++ test/testcases-ryanc/libcss/testutils.rs
@@ -0,0 +1,66 @@
+//////////////////////////////////////////////////////////////////////
+//
+// Filename         : testutils.rs
+// Author           : Ryan Choi
+// Created on       : Monday, 28 May 2013
+// Last Modified on : Monday, 28 May 2013
+// Version          : 1.00
+// Title            :
+//
+//////////////////////////////////////////////////////////////////////
+#[link(name="testutils", vers="0.1", author="ryanc")];
+#[crate_type="lib"];
+
+extern mod core;
+
+use core::io::*;
+
+pub type line_func = ~extern fn(data: ~str, pw: line_ctx) -> bool;
+
+pub enum line_ctx {
+    Csdetect(@mut line_ctx_csdetect),
+    Number(@mut line_ctx_number),
+    Lex(@mut line_ctx_lex)
+}
+
+pub struct line_ctx_csdetect {
+    buflen: uint,  // FIXME: not needed
+    bufused: uint, // FIXME: not needed
+    buf: ~[u8],
+    enc: ~str,
+    indata: bool,
+    inenc: bool
+}
+
+pub struct line_ctx_number{
+    buf: ~[u8],
+    exp: ~[u8],
+    indata: bool,
+    inexp: bool
+}
+
+pub struct line_ctx_lex {
+    buflen: uint
+}
+
+pub fn css__parse_filesize(filename: &str) -> uint {
+    let r: @Reader = io::file_reader(&Path(filename)).get();
+    r.seek(0, SeekEnd);
+    let len = r.tell();
+    return len;
+}
+
+pub fn css__parse_testfile(filename: &str, callback: line_func, pw: line_ctx) -> bool {
+    let reader = result::get(&io::file_reader(&Path(filename)));
+    while !reader.eof() {
+        let line = reader.read_line();
+
+        if !line.is_empty() {
+            if !(*callback)(line, pw) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
