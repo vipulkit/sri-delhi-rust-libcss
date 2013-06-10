15731cfcf583faf22e8dc2a733bae42334ed9261
abhijeetgaiha <abhijeet.gaiha@gmail.com>
2013-06-10 13:15:36 +0530

    Renamed make_test.sh make.sh

--- test/make.sh
+++ test/make.sh
@@ -1,26 +1,26 @@
 #! /bin/sh
 
 # do clean 
-#rm -rf *.so *.o test_aliases test_parserutils_filter test_parserutils_inputstream test_parserutils test_riconv test_wapcaplet
+rm -rf *.so *.o test_aliases test_parserutils_filter test_parserutils_inputstream test_parserutils test_riconv test_wapcaplet
 
-#make test executables
-#export LD_LIBRARY_PATH=.:./..:$LD_LIBRARY_PATH
-#rustc test.rc
+make test executables
+export LD_LIBRARY_PATH=.:./..:$LD_LIBRARY_PATH
+rustc test.rc
 
 #rustc test_aliases.rs -L . -L ../libparserutils
 #rustc test_parserutils_filter.rs -L . -L ../libparserutils
 #rustc test_parserutils_inputstream.rs -L . -L ../libparserutils -L ../libcss -L ../libwapcaplet
 #rustc test_parserutils.rs -L . -L ../libparserutils -L ../libcss -L ../libwapcaplet
-#rustc -L ./.. test_riconv.rs -L .
+rustc -L ./.. test_riconv.rs -L .
 #rustc test_wapcaplet.rs -L . -L ../libwapcaplet
 #rustc test_csdtect.rs -L . -L ../libparserutils -L ../libcss -L ../libwapcaplet
 #rustc test_css_stylesheet.rs -L . -L ../libcss -L ../libwapcaplet -L ../libparserutils/
-#rustc propstrings_perf.rs -L . -L ../libwapcaplet -L ../libcss -L ../libparserutils/
+rustc propstrings_perf.rs -L . -L ../libwapcaplet -L ../libcss -L ../libparserutils/
 #rustc test_lexer_chunks.rs -L . -L ../libparserutils -L ../libcss -L ../libwapcaplet
-#rustc --test test_lexer_chunks_file.rs -L . -L ../libparserutils -L ../libcss -L ../libwapcaplet
-#cd libcss
-#./make_test.sh
-#cd ..
-#cd libparserutils
-#./make_test.sh
-#cd ..
+rustc --test test_lexer_chunks_file.rs -L . -L ../libparserutils -L ../libcss -L ../libwapcaplet
+cd libcss
+./make.sh
+cd ..
+cd libparserutils
+./make.sh
+cd ..
