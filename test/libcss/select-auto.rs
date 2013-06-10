4e17c0e2e6d00903ec5684ede6a0779a590473ed
Vipul Goel <vipulkit@vipulgoel.org>
2013-06-10 10:49:39 +0530

    added functions in slect test case - select-auto

--- test/libcss/select-auto.rs
+++ test/libcss/select-auto.rs
@@ -12,7 +12,7 @@ use css::include::types::*;
 //use css::include::font_face::*;
 //use css::bytecode::bytecode::*;
 use css::utils::errors::*;
-//use css::select::common::*;
+use css::select::common::*;
 //use css::select::dispatch::*;
 //use css::stylesheet::*;
 
@@ -125,7 +125,7 @@ pub fn select_test(file:~str) {
 
 pub fn handle_line(data:&str , ctx:@mut line_ctx) -> bool {
 	let mut error : css_error ;
-	let mut len = unsafe { ctx.sheets.len() -1 } ;
+	let mut len : uint ; 
 	if ( (data.len()>0) && (data[0] == ('#' as u8)) ) {
 
 		if( ctx.intree ) {
@@ -149,7 +149,7 @@ pub fn handle_line(data:&str , ctx:@mut line_ctx) -> bool {
 		else if (ctx.insheet) {
 
 			if( (data.len()>6) && (is_string_caseless_equal( data.slice(1,6), "errors")) ){
-
+				len = unsafe { ctx.sheets.len() -1 } ;
 				assert!( 
 						match ctx.sheets[len].sheet.css_stylesheet_data_done() {
 								CSS_OK=>{true},
@@ -164,6 +164,7 @@ pub fn handle_line(data:&str , ctx:@mut line_ctx) -> bool {
 						(data.len()>4) && (is_string_caseless_equal( data.slice(1,4), "user")) ||
 						(data.len()>6) && (is_string_caseless_equal( data.slice(1,6), "author")) ) {
 				
+				len = unsafe { ctx.sheets.len() -1 } ;
 				assert!( 
 						match ctx.sheets[len].sheet.css_stylesheet_data_done() {
 							CSS_OK=>{true},
@@ -172,6 +173,7 @@ pub fn handle_line(data:&str , ctx:@mut line_ctx) -> bool {
 				css__parse_sheet(ctx, data.slice(1,data.len()-1) );
 			}
 			else {
+				len = unsafe { ctx.sheets.len() -1 } ;
 				let mut error = ctx.sheets[len].sheet.css_stylesheet_append_data(data.to_bytes());
 				assert!( match error {
 							CSS_OK=>{true},
@@ -215,6 +217,7 @@ pub fn handle_line(data:&str , ctx:@mut line_ctx) -> bool {
 			css__parse_tree_data(ctx, data.slice(1,data.len()-1) );
 		}
 		else if ( ctx.insheet ) {
+			len = unsafe { ctx.sheets.len() -1 } ;
 			error = ctx.sheets[len].sheet.css_stylesheet_append_data(data.to_bytes());
 			assert!( match error {
 						CSS_OK=>{true},
@@ -233,11 +236,165 @@ pub fn css__parse_expected(ctx:@mut line_ctx, data:&str) {
 
 }
 
-pub fn css__parse_tree(ctx:@mut line_ctx, data:&str) {
+pub fn css__parse_media_list(data:&str , ctx:@mut line_ctx) -> uint {
 
+	// ' '	(0x20)	space (SPC)
+	// '\t'	(0x09)	horizontal tab (TAB)
+	// '\n'	(0x0a)	newline (LF)
+	// '\v'	(0x0b)	vertical tab (VT)
+	// '\f'	(0x0c)	feed (FF)
+	// '\r'	(0x0d)	carriage return (CR)
+	let mut len : uint = 0 ;
+	let mut result : u64 = 0 ;
+	while len < data.len() {
+
+		/* consume a medium */
+		if ( (data[len]!=0x20) && (data[len]!=0x09) && (data[len]!=0x0a) && 
+			 (data[len]!=0x0b) && (data[len]!=0x0c) && (data[len]!=0x0d)  && data.len()>len) {
+			if( data[len]!= (',' as u8)) {
+				len += 1;
+				loop ;
+			}
+		}
+
+		if ( (data.len()>(10+len)) && is_string_caseless_equal( data.slice(len,len+10), "projection") ) {
+			result = result | (CSS_MEDIA_PROJECTION as u64) ;
+		}
+		else if ( (data.len()>(8+len)) && is_string_caseless_equal( data.slice(len,len+8), "handheld") ) {
+			result = result | (CSS_MEDIA_PROJECTION as u64) ;
+		}
+		else if ( (data.len()>(8+len)) && is_string_caseless_equal( data.slice(len,len+8), "embossed") ) {
+			result = result | (CSS_MEDIA_PROJECTION as u64) ;
+		}
+		else if ( (data.len()>(7+len)) && is_string_caseless_equal( data.slice(len,len+7), "braille") ) {
+			result = result | (CSS_MEDIA_PROJECTION as u64) ;
+		}
+		else if ( (data.len()>(6+len)) && is_string_caseless_equal( data.slice(len,len+6), "speech") ) {
+			result = result | (CSS_MEDIA_PROJECTION as u64) ;
+		}
+		else if ( (data.len()>(6+len)) && is_string_caseless_equal( data.slice(len,len+6), "screen") ) {
+			result = result | (CSS_MEDIA_PROJECTION as u64) ;
+		}
+		else if ( (data.len()>(5+len)) && is_string_caseless_equal( data.slice(len,len+5), "print") ) {
+			result = result | (CSS_MEDIA_PROJECTION as u64) ;
+		}
+		else if ( (data.len()>(5+len)) && is_string_caseless_equal( data.slice(len,len+5), "aural") ) {
+			result = result | (CSS_MEDIA_PROJECTION as u64) ;
+		}
+		else if ( (data.len()>(3+len)) && is_string_caseless_equal( data.slice(len,len+3), "tty") ) {
+			result = result | (CSS_MEDIA_PROJECTION as u64) ;
+		}
+		else if ( (data.len()>(3+len)) && is_string_caseless_equal( data.slice(len,len+3), "all") ) {
+			result = result | (CSS_MEDIA_PROJECTION as u64) ;
+		}
+		else if ( (data.len()>(2+len)) && is_string_caseless_equal( data.slice(len,len+2), "tv") ) {
+			result = result | (CSS_MEDIA_PROJECTION as u64) ;
+		}
+		else {
+			// unknown media type
+			io::println("\n Unknown Media type encountered");
+			assert!(false);
+		}
+
+		/* Consume whitespace */
+		while ( (data[len]==0x20) || (data[len]==0x09) || (data[len]==0x0a) || 
+			 	(data[len]==0x0b) || (data[len]==0x0c) || (data[len]==0x0d) ) && data.len()>len {
+				len += 1;
+		}
+
+		/* Stop if we've reached the end */
+		if ( data.len() <= len ) {
+			break;
+		}
+
+		if data[len] == (',' as u8) {
+			len += 1;
+		}
+
+		/* Consume whitespace */
+		while ( (data[len]==0x20) || (data[len]==0x09) || (data[len]==0x0a) || 
+			 	(data[len]==0x0b) || (data[len]==0x0c) || (data[len]==0x0d) ) && data.len()>len {
+				len += 1;
+		}	
+	}
+	
+	ctx.media = result as u32 ;
+	len
+}
+
+pub fn css__parse_pseudo_list(data:&str,ctx:@mut line_ctx) -> uint {
+	/*
+	const char *p = *data;
+	const char *end = p + *len;
+
+	// <pseudo> [ ',' <pseudo> ]* 
+
+	*element = CSS_PSEUDO_ELEMENT_NONE;
+
+	while (p < end) {
+		const char *start = p;
+
+		// consume a pseudo 
+		while (isspace(*p) == false && *p != ',')
+			p++;
+
+		// Pseudo elements 
+		if (p - start == 12 &&
+				strncasecmp(start, "first-letter", 12) == 0)
+			*element = CSS_PSEUDO_ELEMENT_FIRST_LETTER;
+		else if (p - start == 10 &&
+				strncasecmp(start, "first-line", 10) == 0)
+			*element = CSS_PSEUDO_ELEMENT_FIRST_LINE;
+		else if (p - start == 6 &&
+				strncasecmp(start, "before", 6) == 0)
+			*element = CSS_PSEUDO_ELEMENT_BEFORE;
+		else if (p - start == 5 &&
+				strncasecmp(start, "after", 5) == 0)
+			*element = CSS_PSEUDO_ELEMENT_AFTER;
+		else
+			assert(0 && "Unknown pseudo");
+
+		// Consume whitespace 
+		while (p < end && isspace(*p))
+			p++;
+
+		// Stop if we've reached the end 
+		if (p == end || *p != ',')
+			break;
+
+		// Consume comma
+		p++;
+
+		// Consume whitespace 
+		while (p < end && isspace(*p))
+			p++;
+	}
+
+	*data = p;
+	*len = end - p; */
+	0
+}
+
+pub fn css__parse_tree(ctx:@mut line_ctx, data:&str) {
+	/* [ <media_list> <pseudo>? ] ? */
+	ctx.media = CSS_MEDIA_ALL as u32;
+	ctx.pseudo_element = CSS_PSEUDO_ELEMENT_NONE as u32;
+
+	/* Consume any leading whitespace */
+	let mut data2 = data.trim_left();
+
+	let mut length_processed : uint = 0 ;
+	if (data2.len()>0) {
+		length_processed = css__parse_media_list(data2,ctx);
+	}
+
+	if length_processed < data2.len() {
+		css__parse_pseudo_list(data2.slice(length_processed,data2.len()-1),ctx);
+	}
 }
 
 pub fn css__parse_tree_data(ctx:@mut line_ctx, data:&str) {
+
 
 }
 
