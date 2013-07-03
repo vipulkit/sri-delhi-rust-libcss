fa269e2fd466d159dc71d03000312c4bc9e72fba
abhijeetgaiha <abhijeet.gaiha@gmail.com>
2013-07-03 11:57:26 +0530

    Perf fixes to aliases

--- libparserutils/make.sh
+++ libparserutils/make.sh
@@ -1,4 +1,9 @@
 rm -rf *.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 gcc -fPIC -shared input/iconv_wrapper.c -o libiconv_wrapper.so
+cd charset/
+rm -f aliases.rs
+rustc aliases_gen.rs
+./aliases_gen
+cd ..
 rustc -L . parserutils.rc
