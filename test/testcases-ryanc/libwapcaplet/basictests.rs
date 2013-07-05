779d98daf84de6437d37492b427c413fe54d8620
shambhuk <k.shambhu@samsung.com>
2013-07-05 13:13:39 +0530

    Migrated libcss.wapcaplet code to 0.7

--- test/testcases-ryanc/libwapcaplet/basictests.rs
+++ test/testcases-ryanc/libwapcaplet/basictests.rs
@@ -23,140 +23,131 @@ should be more like
 l.lwc_string_isequal(s1)
 
 than
-l.lwc_string_isequal(new_hre.clone(), sub_hre.clone())
+l.lwc_string_isequal(new_hre, sub_hre)
 
 e.g.,
 s1.lwc_string_hash_value()
 
 than
-wapcaplet::lwc_string_hash_value(intern_one.clone());
+wapcaplet::lwc_string_hash_value(intern_one);
 */
 
 extern mod std;
 extern mod wapcaplet;
 
-use std::arc;
 use wapcaplet::*;
 
 fn main() {
-    io::println(fmt!("libwapcaplet"));
+    println(fmt!("libwapcaplet"));
 }
 
-
-fn lwc_string() -> arc::RWARC<~lwc_string> {
-    let s = arc::RWARC(~lwc_string {
+fn lwc_string() -> @mut lwc_string {
+    let s = @mut lwc_string {
         string: ~"",
         hash: 0,
         insensitive: None
-    });
+    };
     return s;
 }
 
 
 #[test]
 fn ops_with_a_context() {
-    let mut lwc = wapcaplet::lwc();
+    let lwc = wapcaplet::lwc();
     let empty = lwc_string();
 
-    do lwc.write |l| {
-        let s1 = l.lwc_intern_string(~"A");
-        assert!(!l.lwc_string_isequal(empty.clone(), s1.clone()));
-    }
+    let s1 = lwc.lwc_intern_string("A");
+    assert!(!lwc.lwc_string_isequal(empty, s1));
 }
 
 #[test]
 fn lwc_intern_string_twice_ok() {
-    let mut lwc = wapcaplet::lwc();
+    let lwc = wapcaplet::lwc();
     let t1 = lwc_string();
     let t2 = lwc_string();
 
-    do lwc.write |l| {
-        let s1= l.lwc_intern_string(~"A");
-        let s2= l.lwc_intern_string(~"B");
-        assert!(!l.lwc_string_isequal(s1.clone(), t1.clone()));
-        assert!(!l.lwc_string_isequal(s2.clone(), t2.clone()));
-    }
+    let s1= lwc.lwc_intern_string("A");
+    let s2= lwc.lwc_intern_string("B");
+    assert!(!lwc.lwc_string_isequal(s1,t1));
+    assert!(!lwc.lwc_string_isequal(s2,t2));
 }
 
 #[test]
 fn lwc_intern_string_twice_same_ok() {
-    let mut lwc = wapcaplet::lwc();
+    let lwc = wapcaplet::lwc();
     let t1 = lwc_string();
     let t2 = lwc_string();
 
-    do lwc.write |l| {
-        let s1= l.lwc_intern_string(~"A");
-        let s2= l.lwc_intern_string(~"A");
-        assert!(!l.lwc_string_isequal(s1.clone(), t1.clone()));
-        assert!(!l.lwc_string_isequal(s2.clone(), t2.clone()));
-    }
+    let s1= lwc.lwc_intern_string("A");
+    let s2= lwc.lwc_intern_string("A");
+    assert!(!lwc.lwc_string_isequal(s1, t1));
+    assert!(!lwc.lwc_string_isequal(s2, t2));
 }
 
 #[test]
 fn ops_with_a_filled_context() {
-    let mut lwc = wapcaplet::lwc();
+    let lwc = wapcaplet::lwc();
 
-    do lwc.write |l| {
         // with_filled_context_setup
-        let intern_one = l.lwc_intern_string(~"one");
-        let intern_two = l.lwc_intern_string(~"two");
-        let intern_three = l.lwc_intern_string(~"three");
-        let intern_YAY = l.lwc_intern_string(~"YAY");
+        let intern_one = lwc.lwc_intern_string("one");
+        let intern_two = lwc.lwc_intern_string("two");
+        let intern_three = lwc.lwc_intern_string("three");
+        let intern_YAY = lwc.lwc_intern_string("YAY");
 
-        assert!(!l.lwc_string_isequal(intern_one.clone(), intern_two.clone()));
-        assert!(!l.lwc_string_isequal(intern_one.clone(), intern_three.clone()));
-        assert!(!l.lwc_string_isequal(intern_two.clone(), intern_three.clone()));
+        assert!(!lwc.lwc_string_isequal(intern_one, intern_two));
+        assert!(!lwc.lwc_string_isequal(intern_one, intern_three));
+        assert!(!lwc.lwc_string_isequal(intern_two, intern_three));
 
         // lwc_interning_works
-        let new_one = l.lwc_intern_string(~"one");
-        assert!(l.lwc_string_isequal(new_one.clone(), intern_one.clone()));
+        let new_one = lwc.lwc_intern_string("one");
+        assert!(lwc.lwc_string_isequal(new_one, intern_one));
 
         // lwc_intern_substring
-        let new_hre = l.lwc_intern_string(~"hre");
-        let sub_hre_option = l.lwc_intern_substring(new_hre.clone(), 1, 3);
+        let new_hre = lwc.lwc_intern_string("hre");
+        let sub_hre_option = lwc.lwc_intern_substring(new_hre, 1, 3);
         match sub_hre_option {
             Some(sub_hre) => {
-                assert!(l.lwc_string_isequal(new_hre.clone(), sub_hre.clone()));
+                assert!(lwc.lwc_string_isequal(new_hre, sub_hre));
             }
             None => {}
         } 
 
         // lwc_string_ref_ok
         // FIXME: needed but not implemented!!!
-        //assert!(l.lwc_string_isequal(l.lwc_string_ref(intern_one.clone()), intern_one.clone()));
+        //assert!(lwc.lwc_string_isequal(lwc.lwc_string_ref(intern_one), intern_one));
 
         // lwc_string_ref_unref_ok
         // FIXME: needed but not implemented!!!
-        //l.lwc_string_ref(intern_one.clone());
-        //l.lwc_string_unref(intern_one.clone());
+        //lwc.lwc_string_ref(intern_one);
+        //lwc.lwc_string_unref(intern_one);
 
 
         // lwc_string_unref_ok
         // FIXME: needed but not implememted!!!
-        // l.lwc_string_unref(intern_one.clone());
+        // lwc.lwc_string_unref(intern_one);
 
         // lwc_string_isequal_ok
-        assert!(!l.lwc_string_isequal(intern_one.clone(), intern_two.clone()));
+        assert!(!lwc.lwc_string_isequal(intern_one, intern_two));
 
         // lwc_string_caseless_isequal_ok1
-        let new_ONE = l.lwc_intern_string(~"ONE");
-        assert!(!l.lwc_string_isequal(intern_one.clone(), new_ONE.clone()));
-        assert!(l.lwc_string_caseless_isequal(intern_one.clone(), new_ONE.clone()));
+        let new_ONE = lwc.lwc_intern_string("ONE");
+        assert!(!lwc.lwc_string_isequal(intern_one, new_ONE));
+        assert!(lwc.lwc_string_caseless_isequal(intern_one, new_ONE));
 
         // lwc_string_caseless_isequal_ok2
-        let new_yay = l.lwc_intern_string(~"yay");
-        assert!(!l.lwc_string_isequal(intern_YAY.clone(), new_yay.clone()));
-        assert!(l.lwc_string_caseless_isequal(intern_YAY.clone(), new_yay.clone()));
+        let new_yay = lwc.lwc_intern_string("yay");
+        assert!(!lwc.lwc_string_isequal(intern_YAY, new_yay));
+        assert!(lwc.lwc_string_caseless_isequal(intern_YAY, new_yay));
 
         // lwc_string_caseless_isequal_bad
-        assert!(!l.lwc_string_caseless_isequal(intern_YAY.clone(), intern_one.clone()));
+        assert!(!lwc.lwc_string_caseless_isequal(intern_YAY, intern_one));
 
         // lwc_extract_data_ok
         // NOTE: not implemented
 
         // lwc_string_hash_value_ok
         // FIXME: this is a function.
-        wapcaplet::lwc_string_hash_value(intern_one.clone());
+        wapcaplet::lwc_string_hash_value(intern_one);
 
         // lwc_string_is_nul_terminated
         // NOTE: not applicable
@@ -165,10 +156,10 @@ fn ops_with_a_filled_context() {
         // NOTE: not applicable
 
         // lwc_intern_substring_bad_size
-        let _err_str = l.lwc_intern_substring(intern_three.clone(), 1, 100);
+        let _err_str = lwc.lwc_intern_substring(intern_three, 1, 100);
 
         // lwc_intern_substring_bad_offset
-        let _err_str = l.lwc_intern_substring(intern_three.clone(), 100, 1);
+        let _err_str = lwc.lwc_intern_substring(intern_three, 100, 1);
 
 
         // lwc_string_iteration
@@ -177,10 +168,9 @@ fn ops_with_a_filled_context() {
 
         // with_filled_context_teardown
         // FIXME: needed but not implemented!!!
-        // l.lwc_string_unref(intern_one.clone());
-        // l.lwc_string_unref(intern_two.clone());
-        // l.lwc_string_unref(intern_three.clone());
-        // l.lwc_string_unref(intern_YAY.clone());
-    }
+        // l.lwc_string_unref(intern_one);
+        // l.lwc_string_unref(intern_two);
+        // l.lwc_string_unref(intern_three);
+        // l.lwc_string_unref(intern_YAY);
 }
 
