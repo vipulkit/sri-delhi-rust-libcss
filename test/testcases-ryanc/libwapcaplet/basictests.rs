75c9225c246960881c97b52c252a9f17eade5c72
iampiyush <pshagl007@gmail.com>
2013-08-12 12:30:42 +0530

    updated wapcaplet test case

--- test/testcases-ryanc/libwapcaplet/basictests.rs
+++ test/testcases-ryanc/libwapcaplet/basictests.rs
@@ -34,6 +34,9 @@ wapcaplet::lwc_string_hash_value(intern_one);
 
 extern mod std;
 extern mod wapcaplet;
+extern mod extra;
+use extra::time;
+use std::io;
 
 use wapcaplet::*;
 
@@ -44,7 +47,7 @@ fn main() {
 fn lwc_string() -> @mut lwc_string {
     let s = @mut lwc_string {
         id:-1,
-        string: @"",
+        string: ~"",
         insensitive: None
     };
     return s;
@@ -53,61 +56,81 @@ fn lwc_string() -> @mut lwc_string {
 
 #[test]
 fn ops_with_a_context() {
-    let lwc = wapcaplet::lwc();
+    let start_time = time::precise_time_ns();
+    let mut lwc = wapcaplet::lwc();
     let empty = lwc_string();
 
-    let s1 = lwc.lwc_intern_string("A");
-    assert!(!lwc.lwc_string_isequal(empty, s1));
+    let mut s1 = lwc.lwc_intern_string("A");
+    assert!(!lwc.lwc_string_isequal(empty, &mut s1));
+    let end_time = time::precise_time_ns();
+
+    let time = end_time as float -start_time as float;
+
+    io::println(fmt!("time taken for ops_with_a_context == %?" , time));
 }
 
 #[test]
 fn lwc_intern_string_twice_ok() {
-    let lwc = wapcaplet::lwc();
+    let start_time = time::precise_time_ns();
+    let mut lwc = wapcaplet::lwc();
     let t1 = lwc_string();
     let t2 = lwc_string();
 
-    let s1= lwc.lwc_intern_string("A");
-    let s2= lwc.lwc_intern_string("B");
-    assert!(!lwc.lwc_string_isequal(s1,t1));
-    assert!(!lwc.lwc_string_isequal(s2,t2));
+    let mut s1= lwc.lwc_intern_string("A");
+    let mut s2= lwc.lwc_intern_string("B");
+    assert!(!lwc.lwc_string_isequal(&mut s1,t1));
+    assert!(!lwc.lwc_string_isequal(&mut s2,t2));
+    let end_time = time::precise_time_ns();
+
+    let time = end_time as float -start_time as float;
+
+    io::println(fmt!("time taken for lwc_intern_string_twice_ok == %?" , time));
 }
 
 #[test]
 fn lwc_intern_string_twice_same_ok() {
-    let lwc = wapcaplet::lwc();
+    let start_time = time::precise_time_ns();
+    let mut lwc = wapcaplet::lwc();
     let t1 = lwc_string();
     let t2 = lwc_string();
 
-    let s1= lwc.lwc_intern_string("A");
-    let s2= lwc.lwc_intern_string("A");
-    assert!(!lwc.lwc_string_isequal(s1, t1));
-    assert!(!lwc.lwc_string_isequal(s2, t2));
+    let mut s1= lwc.lwc_intern_string("A");
+    let mut s2= lwc.lwc_intern_string("A");
+    assert!(!lwc.lwc_string_isequal(&mut s1, t1));
+    assert!(!lwc.lwc_string_isequal(&mut s2, t2));
+    let end_time = time::precise_time_ns();
+
+    let time = end_time as float -start_time as float;
+
+    io::println(fmt!("time taken for lwc_intern_string_twice_same_ok == %?" , time));
 }
 
 #[test]
 fn ops_with_a_filled_context() {
-    let lwc = wapcaplet::lwc();
+    let start_time = time::precise_time_ns();
+    let mut lwc = wapcaplet::lwc();
 
         // with_filled_context_setup
-        let intern_one = lwc.lwc_intern_string("one");
-        let intern_two = lwc.lwc_intern_string("two");
-        let intern_three = lwc.lwc_intern_string("three");
-        let intern_YAY = lwc.lwc_intern_string("YAY");
+        let mut intern_one = lwc.lwc_intern_string("one");
+        let mut intern_two = lwc.lwc_intern_string("two");
+        let mut intern_three = lwc.lwc_intern_string("three");
+        let mut intern_YAY = lwc.lwc_intern_string("YAY");
 
-        assert!(!lwc.lwc_string_isequal(intern_one, intern_two));
-        assert!(!lwc.lwc_string_isequal(intern_one, intern_three));
-        assert!(!lwc.lwc_string_isequal(intern_two, intern_three));
+        assert!(!lwc.lwc_string_isequal(&mut intern_one, &mut intern_two));
+        assert!(!lwc.lwc_string_isequal(&mut intern_one, &mut intern_three));
+        assert!(!lwc.lwc_string_isequal(&mut intern_two, &mut intern_three));
 
         // lwc_interning_works
-        let new_one = lwc.lwc_intern_string("one");
-        assert!(lwc.lwc_string_isequal(new_one, intern_one));
+        let mut new_one = lwc.lwc_intern_string("one");
+        assert!(lwc.lwc_string_isequal(&mut new_one, &mut intern_one));
 
         // lwc_intern_substring
-        let new_hre = lwc.lwc_intern_string("hre");
-        let sub_hre_option = lwc.lwc_intern_substring(new_hre, 1, 3);
+        let mut new_hre = lwc.lwc_intern_string("hre");
+        let mut sub_hre_option = lwc.lwc_intern_substring(&mut new_hre, 1, 3);
         match sub_hre_option {
             Some(sub_hre) => {
-                assert!(lwc.lwc_string_isequal(new_hre, sub_hre));
+                let mut a = sub_hre;
+                assert!(lwc.lwc_string_isequal(&mut new_hre, &mut a));
             }
             None => {}
         } 
@@ -127,20 +150,20 @@ fn ops_with_a_filled_context() {
         // lwc.lwc_string_unref(intern_one);
 
         // lwc_string_isequal_ok
-        assert!(!lwc.lwc_string_isequal(intern_one, intern_two));
+        assert!(!lwc.lwc_string_isequal(&mut intern_one, &mut intern_two));
 
         // lwc_string_caseless_isequal_ok1
-        let new_ONE = lwc.lwc_intern_string("ONE");
-        assert!(!lwc.lwc_string_isequal(intern_one, new_ONE));
-        assert!(lwc.lwc_string_caseless_isequal(intern_one, new_ONE));
+        let mut new_ONE = lwc.lwc_intern_string("ONE");
+        assert!(!lwc.lwc_string_isequal(&mut intern_one, &mut new_ONE));
+        assert!(lwc.lwc_string_caseless_isequal(&mut intern_one, &mut new_ONE));
 
         // lwc_string_caseless_isequal_ok2
-        let new_yay = lwc.lwc_intern_string("yay");
-        assert!(!lwc.lwc_string_isequal(intern_YAY, new_yay));
-        assert!(lwc.lwc_string_caseless_isequal(intern_YAY, new_yay));
+        let mut new_yay = lwc.lwc_intern_string("yay");
+        assert!(!lwc.lwc_string_isequal(&mut intern_YAY, &mut new_yay));
+        assert!(lwc.lwc_string_caseless_isequal(&mut intern_YAY, &mut new_yay));
 
         // lwc_string_caseless_isequal_bad
-        assert!(!lwc.lwc_string_caseless_isequal(intern_YAY, intern_one));
+        assert!(!lwc.lwc_string_caseless_isequal(&mut intern_YAY, &mut intern_one));
 
         // lwc_extract_data_ok
         // NOTE: not implemented
@@ -156,10 +179,10 @@ fn ops_with_a_filled_context() {
         // NOTE: not applicable
 
         // lwc_intern_substring_bad_size
-        let _err_str = lwc.lwc_intern_substring(intern_three, 1, 100);
+        let _err_str = lwc.lwc_intern_substring(&mut intern_three, 1, 100);
 
         // lwc_intern_substring_bad_offset
-        let _err_str = lwc.lwc_intern_substring(intern_three, 100, 1);
+        let _err_str = lwc.lwc_intern_substring(&mut intern_three, 100, 1);
 
 
         // lwc_string_iteration
@@ -172,5 +195,10 @@ fn ops_with_a_filled_context() {
         // l.lwc_string_unref(intern_two);
         // l.lwc_string_unref(intern_three);
         // l.lwc_string_unref(intern_YAY);
+        let end_time = time::precise_time_ns();
+
+        let time = end_time as float -start_time as float;
+
+        io::println(fmt!("time taken for ops_with_fill_context == %?" , time));
 }
 
