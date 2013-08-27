aaabdc348f79758d672b60d3049cd7b37cca9be2
Vipul Goel <vipulkit@vipulgoel.org>
2013-08-27 11:30:23 +0530

    Adding task based parallelization in lwc string

--- libwapcaplet/wapcaplet.rs
+++ libwapcaplet/wapcaplet.rs
@@ -193,3 +193,111 @@ priv fn lwc()->~lwc {
 
 pub static mut lwc_ref : Option<~lwc>  = None;
 
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+pub enum to_lwc {
+    C_INTERN(~str),
+    C_GET_LENGTH(uint),
+    C_GET_DATA(uint),
+    C_INTERN_SUBSTRING(uint,uint,uint),
+    C_INTERN_CASELESS(uint),
+    C_IS_CASELESS_EQUAL(uint)
+    C_TERMINATE
+}
+
+pub struct from_lwc {
+    R_INTERN(uint),
+    R_GET_LENGTH(uint),
+    R_GET_DATA(~str),
+    R_INTERN_SUBSTRING(uint),
+    R_INTERN_CASELESS(uint),
+    R_TERMINATE  // Not Required
+}
+
+pub static mut lwc_ref : Option<~lwc_wrapper>  = None;
+
+pub struct lwc_wrapper {
+    thread_handle : Option<Chan< (to_lwc,Port<from_lwc>) >)>
+}
+
+pub fn create_lwc_thread() {
+    unsafe{
+        if lwc_ref.is_none() { 
+            // create task  and assign handle to it 
+        }
+    }
+}
+
+
+impl lwc {
+
+    #[inline]
+    pub fn dolower(c: u8 ) -> u8 {
+        if (c > 64 && c < 91) {
+              return (c  + 32) ;
+        }
+        c
+    }
+
+    #[inline]
+    fn to_lower(string:&str) -> ~str{
+        let mut lower : ~[u8] = ~[];
+        let len = string.len();
+        lower.reserve(len);
+        let mut c = 0;
+        let mut ch : u8;
+        while c < len {
+            ch = string[c] as u8;
+            unsafe {
+                if (ch > 64 && ch < 91) {
+                    lower.push_fast(ch + 32);
+                } else {
+                    lower.push_fast(ch);
+                }
+            }   
+            c += 1;
+        }
+        str::from_bytes_owned(lower)
+    }
+
+    #[inline]
+    pub fn lwc_intern_string(&mut self, val: &str) -> uint {
+
+    }
+
+
+    #[inline]
+    pub fn lwc_string_isequal(&mut self, str1: uint , str2: uint) -> bool {
+        str1 == str2
+    }
+
+    #[inline]
+    pub fn lwc_string_caseless_isequal(&mut self, str1: uint , str2: uint) ->bool {
+                        
+    }
+
+    #[inline]
+    pub fn lwc_intern_caseless_string(&mut self , string: uint) {
+
+    }   
+
+    
+    #[inline]
+    pub fn lwc_intern_substring(&mut self , substring_to_intern: uint , ssoffset: u32, sslen: u32) -> Option<uint> {
+    }
+
+    #[inline]
+    pub fn lwc_string_length(&self, string:uint) -> uint {
+    }
+        
+    #[inline]
+    pub fn lwc_string_data(&self, string:uint) -> ~str {
+    }
+
+    
+} // impl wapcaplet
+
+/////////////////////////////////////////////////////////////////////////////////////////
